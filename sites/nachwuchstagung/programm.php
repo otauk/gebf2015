ccda572c0f106c9f3d8daa6d73fe162f8513bbbb
KUATOproject <KUATOimac@admin.lehrer.ruhr-uni-bochum.de>
2014-12-02 15:58:55 +0100

    Daten für NWT geändert auf 30.01.2015

--- sites/nachwuchstagung/programm.php
+++ sites/nachwuchstagung/programm.php
@@ -1,12 +1,12 @@
 <?php $thisPage="nwt_programm";?>
-<?php include("../../header.php") ?>			
+<?php include("../../header.php") ?>
 			<div class="content">
 				<div class="text">
 <h1>Veranstaltungsprogramm</h1><h2>Montag, 09. März 2015</h2>
 <p>Die Nachwuchstagung startet am Montag um 12:00 Uhr mit einer Begrüßung der Tagungsleiterin Prof. Dr. Katrin Sommer. Hieran schließt sich der Eröffnungsvortrag von <a href="../wissenschaftliches-programm/eingeladene-vortragende.php#koeller">Prof. Dr. Olaf Köller</a> zum Thema der Nachwuchstagung an. Ergänzt durch drei weitere Statements aus der Wissenschaft und Schulpraxis wird damit das heterogene Feld „Zwischen empirischen Daten und unterrichts-praktischer Relevanz“ aufgespannt. Bei der anschließenden Podiumsdiskussion erhalten die Referentinnen und Referenten sowie Tagungsteilnehmerinnen und -teilnehmer die Gelegenheit, die erörterten Punkte gemeinsam weiter zu reflektieren.
 </p>
 <p>
-Weiter geht es mit der Poster-Ausstellung, bei der die Dissertationsprojekte der Teilnehmerinnen und Teilnehmer im Mittelpunkt stehen.  Die Nachwuchswissenschaftlerinnen und -wissenschaftler stellen ihre eigenen Forschungsprojekte gegenseitig vor und tauschen sich darüber aus. Wenn Sie Ihr Dissertationsprojekt mit einem Poster vorstellen möchten, reichen Sie Ihren Posterbeitrag bitte unter „Poster (Nachwuchstagung) <a href="https://www.conftool.com/gebf2015/" target="_blank">hier</a> ein (siehe auch <a href="call-for-abstracts-and-posters.php">„Call for Abstracts and Posters"</a>). 
+Weiter geht es mit der Poster-Ausstellung, bei der die Dissertationsprojekte der Teilnehmerinnen und Teilnehmer im Mittelpunkt stehen.  Die Nachwuchswissenschaftlerinnen und -wissenschaftler stellen ihre eigenen Forschungsprojekte gegenseitig vor und tauschen sich darüber aus. Wenn Sie Ihr Dissertationsprojekt mit einem Poster vorstellen möchten, reichen Sie Ihren Posterbeitrag bitte bitte bis 30.01.2015 unter „Poster (Nachwuchstagung) <a href="https://www.conftool.com/gebf2015/" target="_blank">hier</a> ein (siehe auch <a href="call-for-abstracts-and-posters.php">„Call for Abstracts and Posters"</a>).
 </p>
 <p>
 Am Abend des ersten Tagungstages werden die Teilnehmerinnen und Teilnehmer zusammen mit den Dozentinnen und Dozenten der Nachwuchstagung zu einem Conference Dinner auf dem Campus der RUB eingeladen. Dieses „Junior meets Senior“-Format bietet allen die Möglichkeit, in einem lockeren und informellen Rahmen Expertinnen und Experten ihres Faches kennenzulernen und von deren reichen Erfahrungen zu profitieren. Das Conference Dinner ist im Teilnehmerbeitrag inbegriffen.
@@ -14,7 +14,7 @@ Am Abend des ersten Tagungstages werden die Teilnehmerinnen und Teilnehmer zusam
 <h2>Dienstag, 10. März 2015</h2>
 <h3>Publikationsworkshop</h3>
 <p>
-Der Publikationsworkshop besteht aus zwei Teilen. Dabei werden zwei unterschiedliche Darstellungsformate in der Wissenschaftskommunikation der Zielgruppe in den Blick genommen: Artikel in wissenschaftlichen und in unterrichtspraktischen Zeitschriften. Die Teilnehmerinnen und Teilnehmer besuchen entsprechend jeweils beide Teile. 
+Der Publikationsworkshop besteht aus zwei Teilen. Dabei werden zwei unterschiedliche Darstellungsformate in der Wissenschaftskommunikation der Zielgruppe in den Blick genommen: Artikel in wissenschaftlichen und in unterrichtspraktischen Zeitschriften. Die Teilnehmerinnen und Teilnehmer besuchen entsprechend jeweils beide Teile.
 <br/><a class="readmore"></a>
 </p>
 <div class="expander">
@@ -24,7 +24,7 @@ Der Publikationsworkshop besteht aus zwei Teilen. Dabei werden zwei unterschiedl
 Die Teilnehmerinnen und Teilnehmer können hier wählen, ob sie an der wissenschaftlichen Publikation empirisch-quantitativer ODER empirisch-qualitativer Methoden und Ergebnisse arbeiten möchten. Bitte geben Sie dafür bei der <a href="https://www.conftool.com/gebf2015/" target="_blank">Anmeldung zur Nachwuchstagung</a> an, ob Sie den Teil 1 als „Publizieren auf Basis empirisch-qualitativer Forschungsmethoden“ (geleitet durch <a href="../wissenschaftliches-programm/eingeladene-vortragende.php#glaeser">Prof. Dr. Michaela Gläser-Zikuda</a>, Universität Erlangen-Nürnberg) ODER als „Publizieren auf Basis empirisch-quantitativer  Forschungsmethoden“ (geleitet durch <a href="../wissenschaftliches-programm/eingeladene-vortragende.php#luedtke">Prof. Dr. Oliver Lüdtke</a>, Institut für die Pädagogik der Naturwissenschaften, Kiel) besuchen möchten.
 </p>
 <p>
-Publizieren auf Basis empirisch-qualitativer Forschungsmethoden: Der Workshop widmet sich der Frage, welche Aspekte und Kriterien insbesondere für das Publizieren empirisch qualitativer Studien bedacht werden sollten. Zunächst werden grundlegende Fragen der Güte qualitativer Sozialforschung erläutert und darauf aufbauend anhand von Beispielstudien aus der empirisch qualitativen Bildungsforschung Grundprinzipien für die Gestaltung eines Theorie- und Ergebnisteils für eine Publikation (mit peer-Review-Verfahren) diskutiert. 
+Publizieren auf Basis empirisch-qualitativer Forschungsmethoden: Der Workshop widmet sich der Frage, welche Aspekte und Kriterien insbesondere für das Publizieren empirisch qualitativer Studien bedacht werden sollten. Zunächst werden grundlegende Fragen der Güte qualitativer Sozialforschung erläutert und darauf aufbauend anhand von Beispielstudien aus der empirisch qualitativen Bildungsforschung Grundprinzipien für die Gestaltung eines Theorie- und Ergebnisteils für eine Publikation (mit peer-Review-Verfahren) diskutiert.
 </p>
 <p>
 Folgende Aspekte werden thematisiert:
@@ -37,7 +37,7 @@ Folgende Aspekte werden thematisiert:
  </p>
 <p>
 Publizieren auf Basis empirisch-quantitativer Forschungsmethoden: Dieser Teil widmet sich der Frage, was bei der Anfertigung eines Methoden- und Ergebnisteils für eine wissenschaftliche Publikation beachtet werden muss. Es werden anhand einer Beispielstudie aus der Bildungsforschung Grundprinzipien für die Gestaltung eines Ergebnisteils diskutiert.
-Folgende Aspekte werden behandelt: 
+Folgende Aspekte werden behandelt:
 <ul>
 <li>nachvollziehbare Beschreibung der verwendeten statistischen Analyseverfahren </li>
 <li>Qualität der Messverfahren (Reliabilität, Validität)</li>
@@ -55,14 +55,14 @@ In diesem Teil werden die Teilnehmerinnen und Teilnehmer in Fächergruppen (MINT,
 </div>
 <h3>Methodenworkshop „Einführung in das Statistikprogrammpaket R“</h3>
 <p>
-Dieser Workshop richtet sich an Nachwuchswissenschaftlerinnen und –wissenschaftler, die am Beginn ihres Promotionsprojektes stehen. Er vermittelt eine Einführung in das Statistikprogrammpaket R anhand einfacher statistischer Analyseverfahren. Geleitet wird der Workshop durch <a href="../wissenschaftliches-programm/eingeladene-vortragende.php#goldhammer">Prof. Dr. Frank Goldhammer</a> Deutsches Institut für Internationale Pädagogische Forschung, Frankfurt. 
+Dieser Workshop richtet sich an Nachwuchswissenschaftlerinnen und –wissenschaftler, die am Beginn ihres Promotionsprojektes stehen. Er vermittelt eine Einführung in das Statistikprogrammpaket R anhand einfacher statistischer Analyseverfahren. Geleitet wird der Workshop durch <a href="../wissenschaftliches-programm/eingeladene-vortragende.php#goldhammer">Prof. Dr. Frank Goldhammer</a> Deutsches Institut für Internationale Pädagogische Forschung, Frankfurt.
 <br/><a class="readmore2"></a>
 </p>
 <div class="expander2">
 <h1>Workshop „R-Einführung“ am 10.03.2015</h1>
 <p>
 Ziel des Workshops ist die Vermittlung grundlegender Fertigkeiten im Umgang mit der Statistik-Umgebung R. Dazu zählt das Arbeiten mit verschiedenen Datenobjekten und
--typen, der Datenimport/-export, die Benutzung von grundlegenden deskriptiven und inferenzstatistischen Funktionen u.a.m. 
+-typen, der Datenimport/-export, die Benutzung von grundlegenden deskriptiven und inferenzstatistischen Funktionen u.a.m.
 </p>
 <p>
 Ein Teil der Veranstaltung wird sich mit R-Paketen und -Funktionen befassen, die speziell für Datenanalysen und -modellierungen in der quantitativen empirischen Forschung nützlich sind. Ein Schwerpunkt wird dabei die Skalen- und Itemanalyse (z.B. Fragebogen oder Tests) bilden, unter Verwendung von Datenbeispielen aus dem Bereich des Large-Scale Assessment (z.B. PISA, PIAAC).
@@ -73,7 +73,7 @@ Der Workshop richtet sich an alle, die R kennenlernen wollen. R-Vorkenntnisse si
 </div>
 
 <h3>Methodenworkshop „Einführung in die Latent Class Analysis“</h3>
-Dieser Workshop richtet sich an methodisch fortgeschrittene Promovierende und führt in die Latent Class Analysis ein. Geleitet wird der Workshop von <a href="../wissenschaftliches-programm/eingeladene-vortragende.php#uenlue">Prof. Dr. Ali Ünlü</a>, Technische Universität München. 
+Dieser Workshop richtet sich an methodisch fortgeschrittene Promovierende und führt in die Latent Class Analysis ein. Geleitet wird der Workshop von <a href="../wissenschaftliches-programm/eingeladene-vortragende.php#uenlue">Prof. Dr. Ali Ünlü</a>, Technische Universität München.
 <br/><a class="readmore3"></a>
 </p>
 <div class="expander3">
@@ -82,7 +82,7 @@ Dieser Workshop richtet sich an methodisch fortgeschrittene Promovierende und fü
 Das Diagnostizieren von Problemen ist allgegenwärtig. Beispielsweise das Feststellen der Stärken und Schwächen von Schülerinnen und Schülern in einer Schulklasse in einem Wissensbereich und das Bestimmen effektiver Bildungsmaßnahmen. Ziel ist in der Regel die Erstellung eines differenzierten Profils des Individuums bezüglich interessierender Charakteristika wie etwa Problemlösestrategien der Schülerinnen und Schüler.
 </p>
 <p>
-Der Workshop führt durch Vortragsphasen, Gruppenübungen und Softwaredemonstration in die Latent Class Analysis ein. Diese Methode dient zur modellbasierten Clusterung und Klassifikation basierend auf latenten Typologien. Andererseits werden auch Grundannahmen und Ansätze der Item-Response-Theorie und der diagnostischen Modellierungsverfahren kurz vorgestellt. Eine historische und wissenschaftstheoretische Einordnung der Konzepte runden den Workshop ab. 
+Der Workshop führt durch Vortragsphasen, Gruppenübungen und Softwaredemonstration in die Latent Class Analysis ein. Diese Methode dient zur modellbasierten Clusterung und Klassifikation basierend auf latenten Typologien. Andererseits werden auch Grundannahmen und Ansätze der Item-Response-Theorie und der diagnostischen Modellierungsverfahren kurz vorgestellt. Eine historische und wissenschaftstheoretische Einordnung der Konzepte runden den Workshop ab.
 </p>
 <p>
 Die Teilnehmerinnen und Teilnehmer erwerben durch die Veranstaltung ein Grundverständnis für psychometrische Denkweisen und für den sowohl theoretischen als auch praktischen Umgang mit latenten Variablen. Die Teilnehmerinnen und Teilnehmer sollten ihren eigenen Laptop mitbringen, mit einer aktuellen Demoversion der Software Latent GOLD (http://www.statisticalinnovations.com/products/latentgold.html).
@@ -91,20 +91,20 @@ Die Teilnehmerinnen und Teilnehmer erwerben durch die Veranstaltung ein Grundver
 
 <h3>Abschluss der Nachwuchstagung</h3>
 <p>
-Am späteren Nachmittag des zweiten Tagungstages erfolgt für alle als Ausblick ein Vortrag über das Forschungsprojekt „Bildungsmedien Online“. Der Vortrag von <a href="wp_vortragende.php">Christian Fey</a>, Universität Augsburg, nimmt den neu aufkommen-den Markt für kostenlose Lehrmaterialien im Internet in den Blick sowie Kriterien für die Bewertung von diesem Material. Hieran schließt sich bis ca. 18 Uhr die Abschlussdiskussion der Tagung an. 		
-<br/><a class="readmore4"></a>	
+Am späteren Nachmittag des zweiten Tagungstages erfolgt für alle als Ausblick ein Vortrag über das Forschungsprojekt „Bildungsmedien Online“. Der Vortrag von <a href="wp_vortragende.php">Christian Fey</a>, Universität Augsburg, nimmt den neu aufkommen-den Markt für kostenlose Lehrmaterialien im Internet in den Blick sowie Kriterien für die Bewertung von diesem Material. Hieran schließt sich bis ca. 18 Uhr die Abschlussdiskussion der Tagung an.
+<br/><a class="readmore4"></a>
 </p>
 <div class="expander4">
 <h1>Vortrag am 10.03.2015</h1>
 <p>
 „Möglichkeiten des Transfers fachwissenschaftlicher und fachdidaktischer Forschung und ihrer Ergebnisse in außerwissenschaftliche relevante Kontexte (Bildungspolitik, öffentlich-medialer Diskurs, Unterrichtspraxis, etc.). Vorgestellt und diskutiert am Beispiel des Forschungsprojekts `Bildungsmedien Online´ der Lehrstühle für Pädagogik und für Schulpädagogik der Universität Augsburg“.</p>
 <p>
-Der Vortrag beschäftigt sich mit den Chancen (und Grenzen) des Anschlusses von fachlicher bzw. fachdidaktisch motivierter empirischer Forschung für außerwissenschaftliche Diskurse bzw. Handlungsfelder (z.B. Bildungspolitik, öffentliche-medialer Diskurs, Unterrichtspraxis, Lehr-/Lernmittelproduktion und Distribution, etc.). 
+Der Vortrag beschäftigt sich mit den Chancen (und Grenzen) des Anschlusses von fachlicher bzw. fachdidaktisch motivierter empirischer Forschung für außerwissenschaftliche Diskurse bzw. Handlungsfelder (z.B. Bildungspolitik, öffentliche-medialer Diskurs, Unterrichtspraxis, Lehr-/Lernmittelproduktion und Distribution, etc.).
 </p>
 <p>
 Ausgehend von Erfahrungen, die in dem dreijährigen Forschungsprojekt "Bildungsmedien Online" gemacht wurden, das in vielfältiger Weise Anklang in den unterschiedlichen genannten Feldern gefunden hat (z.B in Zeitschriften-, Fernseh- und Radiobeiträgen, in bildungspolitisch geführten Auseinandersetzungen in politischen Gremien sowie bei Akteuren der Schulaufsicht, in der Lehrerfortbildung), werden Überlegungen zur strukturellen Ausrichtung von Forschungsprozessen (Begründungszusammenhang, Kontextualisierung) und auch zur Präsentation und Distribution ihrer Ergebnisse angestellt. Dabei geht es darum, begünstigende Faktoren zu identifizieren, die einen Transfer von wissenschaftlichen Forschungsergebnissen in die genannten Kontexte zu erleichtern.
-</p></div> 
+</p></div>
 				</div>
 </div>
 		</div>
-<?php include("../../footer.php") ?>			
+<?php include("../../footer.php") ?>
