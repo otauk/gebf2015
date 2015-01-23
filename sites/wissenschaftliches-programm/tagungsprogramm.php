258bc7488081bd470ead1bb8b994ecc226c259d4
KUATOproject <manuel.buczka@rub.de>
2015-01-23 13:08:38 +0100

    tagungsprogramm dazu

---
+++ sites/wissenschaftliches-programm/tagungsprogramm.php
@@ -0,0 +1,130 @@
+<?php $thisPage="tagungsprogramm";?>
+<?php include("../../header.php") ?>
+			<div class="content">
+				<div class="text">
+					<h1>Tagungsprogramm</h1>
+					<h2>Mittwoch, 11.03.2015</h2>
+					<table class="tbl">
+						<tr>
+							<th>Zeit</th>
+							<th>Veranstaltung</th>
+						</tr>
+						<tr>
+							<td>9:00 – 10:45 Uhr	</td>
+							<td><a href="workshops.php">Workshops</a> der Haupttagung</td>
+						</tr>
+						<tr>
+							<td>11:00 – 12:00 Uhr	</td>
+							<td>Eröffnungsveranstaltung</td>
+						</tr>
+						<tr>
+							<td>12:00 – 13:00 Uhr	</td>
+							<td><a href="mittagsvorlesungen.php">Mittagsvorlesung</a> von <a href="eingeladene-vortragende.php#sumfleth"> Prof.Dr. Elke Sumfleth</a></td>
+						</tr>
+						<tr>
+							<td>13:00 – 14:00 Uhr	</td>
+							<td>Mittagspause</td>
+						</tr>
+						<tr>
+							<td>14:00 – 15:45 Uhr	</td>
+							<td>Symposien und Einzelbeiträge</td>
+						</tr>
+						<tr>
+							<td>15:45 – 16:15 Uhr</td>
+							<td>	Poster-Session und Kaffeepause</td>
+						</tr>
+						<tr>
+							<td>16:15 – 18:00 Uhr</td>
+							<td>Symposien und Einzelbeiträge</td>
+						</tr>
+						<tr>
+							<td>18:00 – open end	</td>
+							<td>Sektempfang</td>
+						</tr>
+					</table>
+					<h2>Donnerstag, 12.03.2015</h2>
+					<table class="tbl">
+						<tr>
+							<th>Zeit</th>
+							<th>Veranstaltung</th>
+						</tr>
+						<tr>
+							<td>9:00 – 10:45 Uhr	</td>
+							<td>Symposien und Einzelbeiträge</td>
+						</tr>
+						<tr>
+							<td>10:45 – 11:15 Uhr	</td>
+							<td>Poster-Session und Kaffeepause</td>
+						</tr>
+						<tr>
+							<td>11:15 – 13:00 Uhr	</td>
+							<td>Symposien und Einzelbeiträge</td>
+						</tr>
+						<tr>
+							<td>13:00 – 14:00 Uhr	</td>
+							<td>Mittagspause</td>
+						</tr>
+						<tr>
+							<td>14:00 – 15:00 Uhr	</td>
+							<td><a href="mittagsvorlesungen.php">Mittagsvorlesung</a> von <a href="eingeladene-vortragende.php#becker">Prof. Dr. Rolf Becker</a></td>
+						</tr>
+						<tr>
+							<td>15:00 – 15:30 Uhr</td>
+							<td>	Poster-Session und Kaffeepause</td>
+						</tr>
+						<tr>
+							<td>15:30 – 17:15 Uhr</td>
+							<td>Symposien und Einzelbeiträge</td>
+						</tr>
+						<tr>
+							<td>17:30 – 19:00 Uhr	</td>
+							<td>Mitgliederversammlung</td>
+						</tr>
+						<tr>
+							<td>19:30 – open end	</td>
+							<td><a href="../rahmenprogramm/gesellschaftsabend.php">Gesellschaftsabend</a> (mit Verleihung des Posterpreises)</td>
+						</tr>
+					</table>
+					<h2>Freitag, 13.03.2015</h2>
+					<table class="tbl">
+						<tr>
+							<th>Zeit</th>
+							<th>Veranstaltung</th>
+						</tr>
+						<tr>
+							<td>9:00 – 10:45 Uhr	</td>
+							<td>Symposien und Einzelbeiträge</td>
+						</tr>
+						<tr>
+							<td>10:45 – 11:15 Uhr	</td>
+							<td>Kaffeepause</td>
+						</tr>
+						<tr>
+							<td>11:15 – 13:00 Uhr	</td>
+							<td>Symposien und Einzelbeiträge</td>
+						</tr>
+						<tr>
+							<td>13:00 – 14:00 Uhr	</td>
+							<td>Mittagspause</td>
+						</tr>
+						<tr>
+							<td>14:00 – 15:00 Uhr	</td>
+							<td><a href="mittagsvorlesungen.php">Mittagsvorlesung</a> von <a href="eingeladene-vortragende.php#maagmerki">Prof. Dr. Katharina Maag Merki</a></td>
+						</tr>
+						<tr>
+							<td>15:00 – 15:30 Uhr</td>
+							<td>Kaffeepause</td>
+						</tr>
+						<tr>
+							<td>15:30 – 17:15 Uhr</td>
+							<td>Symposien und Einzelbeiträge</td>
+						</tr>
+						<tr>
+							<td>17:30 – 19:00 Uhr	</td>
+							<td>Abschlussveranstaltung</td>
+						</tr>
+					</table>
+				</div>
+</div>
+		</div>
+<?php include("../../footer.php") ?>
