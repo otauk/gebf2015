5197e100f665307ba9f1a7273ac3e461bace1766
KUATOproject <manuel.buczka@rub.de>
2015-03-17 12:18:08 +0100

    Update Posterpreis Nachwuchstagung

---
+++ sites/nachwuchstagung/posterpreis.php
@@ -0,0 +1,32 @@
+<?php $thisPage="nwt_cfp";?>
+<?php include("../../header.php") ?>
+			<div class="content">
+				<div class="text">
+<h1>Posterpreis</h1>
+<p>
+Alle Teilnehmerinnen und Teilnehmer der GEBF-Nachwuchstagung waren eingeladen, einen eigenen Posterbeitrag für die Poster-Session am 09.03.2015 einzureichen.
+Folgende Nachwuchswissenschaftler haben mit ihren Postern den diesjährigen Posterpreis erhalten:
+</p>
+<p>
+<span class="bigger">Miriam Rest, M.Ed.</span><br/>
+Ruhr-Universität Bochum, Didaktik der Biologie (Prof. Dr. Wolfgang H. Kirchner)<br/>
+<a href="../../Miriam%20Rest%20Poster%20GEBF%202015.pdf">„Lehramtsstudierende im Praktikum: Der Einfluss fachwissenschaftlicher Leistung und pädagogischer Erfahrung auf die Kompetenzentwicklung.“</a>
+</p>
+<p>
+<span class="bigger">Jennifer Wengler, M.Ed.</span><br/>
+Ruhr-Universität Bochum, Didaktik der romanischen Literaturen (Prof. Dr. Lieselotte Steinbrügge)<br/>
+<a href="../../Poster%20GEBF_Wengler.pdf">„Mündliche Fehlerkorrektur und Schüleremotionen.“</a>
+</p>
+<p>
+<span class="bigger">Jan Schröder, M.A.</span><br/>
+TU Dortmund, Institut für Schulentwicklungsforschung (Prof. Dr. Wilfried Bos)<br/>
+<a href="../../Poster_Nachwuchstagung_Schr%C3%B6der.pdf">„Warum scheitern Kinder am Ganztagsgymnasium? – Eine Analyse möglicher Ursachen.“</a>
+</p>
+Der Posterpreis wurde von der RUB Research School gestiftet. Er wurde im Sinne eines „Publikumspreises“ auf der Nachwuchstagung vergeben, in einem Verfahren, das die fachübergreifende Kommunikation der Projekte unterstützte. Wir danken der RUB Research School herzlich für die Stiftung der Preisgelder.
+<img src="http://www.research-school.rub.de/fileadmin/graphics/Logo/logo%202012/Logo-RURS-rgb.png" class="shadow center w50"/>
+
+</p>
+</div>
+				</div>
+</div>
+<?php include("../../footer.php") ?>
